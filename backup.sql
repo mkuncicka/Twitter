3ac962a8df409e2bb63a62a9656913124389c8a7
mkuncicka <mkuncicka@gmail.com>
2016-07-02 13:37:27 +0200

    Database backup

--- backup.sql
+++ backup.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(60) DEFAULT NULL,
+  `tweet_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tweet_id` (`tweet_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`tweet_id`) REFERENCES `tweets` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tweets`
 --
 
@@ -24,12 +50,12 @@ DROP TABLE IF EXISTS `tweets`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tweets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(140) DEFAULT NULL,
+  `content` varchar(140) CHARACTER SET utf8 DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +64,7 @@ CREATE TABLE `tweets` (
 
 LOCK TABLES `tweets` WRITE;
 /*!40000 ALTER TABLE `tweets` DISABLE KEYS */;
+INSERT INTO `tweets` VALUES (1,'Pierwszy Tweet Tomka',3),(3,'Drugi tweet tomka',3);
 /*!40000 ALTER TABLE `tweets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +83,7 @@ CREATE TABLE `users` (
   `is_active` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,6 +92,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'ala@test.pl','$2y$10$mNWn2SbHf82lx1A/OBm4U.b4ISbRq.CnFkSRKQJ4CSI9yxTwHarc2','To jest ala',1),(2,'magda@test.pl','$2y$10$RRQqKOlwdTX6/x0bUH9SUuJSON8Ls3lxM7toCc6papSPJT7/5uOEi','To jest magda',1),(3,'tomek@test.pl','$2y$10$XUZIi4nFpgRdj.wMCsFQW.FgHWJWIvChceyFY9DNSKslCCZQsVCGi','To jest tomek',1),(5,'kasia@test.pl','$2y$10$tUChbos6U2JKfAU4lPCzTOQoZQ3XN1KGEJF7Wz3mbgYzjSPi.heaG','To jest opis kasi',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -77,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-01 15:18:44
+-- Dump completed on 2016-07-02 11:35:57
